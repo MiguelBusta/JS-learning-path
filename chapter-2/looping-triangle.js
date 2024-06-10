37a199a77ff00d6f8367628c38d4d17738026102
Miguel Bustamante <110345846+MiguelBusta@users.noreply.github.com>
2024-06-10 10:54:04 -0600

    Chapter 2: Add looping-triangle exercise

---
+++ chapter-2/looping-triangle.js
@@ -0,0 +1,26 @@
+/*
+* Looping Triangle
+
+TODO: Write a loop that makes seven calls to console.log to output the following triangle:
+
+#
+##
+###
+####
+#####
+######
+#######
+
+It may be useful to know that you can find the length of a string by writing .length after it.
+let abc = "abc";
+console.log(abc.length);
+
+/ 3
+*/
+
+triangle = "#"
+
+for (i = 0; i < 7; i++){
+    console.log(triangle);
+    triangle += "#"
+}
